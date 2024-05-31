cbb758918ecfd1841a017c83f2c767fc28bf3620
shimashimau <shuriken233@gmail.com>
2024-05-31 13:06:36 +0200

    gdf

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,28 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 import { getAllPostsForHome } from "@/lib/api";
 
 export async function GET(request: NextRequest) {
-    try {
-        // const { searchParams } = new URL(request.url);
-        const { searchParams } = request.nextUrl;
-        console.log('Search params:', searchParams.toString());
-        
-        const after = searchParams.get('after');
-        const allPosts = await getAllPostsForHome(false, after); // Adjust based on your needs, e.g., preview mode from query params
-
-        return new NextResponse(JSON.stringify(allPosts), {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    } catch (error) {
-        console.error('Failed to fetch posts:', error);
-        return new NextResponse(JSON.stringify({ error: 'Failed to fetch posts' }), {
-            status: 500,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-    }
+  // const { searchParams } = new URL(request.url);
+  const { searchParams } = request.nextUrl;
+  const after = searchParams.get("after");
+  try {
+    const allPosts = await getAllPostsForHome(false, after); // Adjust based on your needs, e.g., preview mode from query params
+    return new NextResponse(JSON.stringify(allPosts), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Failed to fetch posts:", error);
+    return new NextResponse(
+      JSON.stringify({ error: "Failed to fetch posts" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
