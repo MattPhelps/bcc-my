e79ee368b0b9eaa7436c6520db38948475e31161
Matt Phelps <38656131+MattPhelps@users.noreply.github.com>
2024-06-01 18:28:48 +0200

    Delete inkgen directory

--- inkgen/src/types/ideas.ts
+++
@@ -1,9 +0,0 @@
-export type Ideas = {
-  id: number;
-  title: string;
-  label: string;
-  image: string;
-  description: string;
-  slug: string;
-  count: number;
-};
--- inkgen/src/types/styles.ts
+++
@@ -1,7 +0,0 @@
-export type Styles = {
-  id: number;
-  title: string;
-  image: string;
-  description: string;
-  href: string;
-};
