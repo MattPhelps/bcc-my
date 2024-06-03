decc7fd764a05d786accc228e413d59aa503670b
Matt Phelps <matt@leandme.com>
2024-06-03 16:59:48 +0200

    design changes

--- src/components/Design/DesignRender/index.tsx
+++ src/components/Design/DesignRender/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const DesignRender = () => (
   <div>
-    <h3 className="text-white font-medium mb-2 p-1 mt-4">Style</h3>
-           <p>pussy</p>
+    
+           
   </div>
 );
 
