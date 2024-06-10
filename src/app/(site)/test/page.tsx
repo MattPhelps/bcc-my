918be35592d7e2a1a28ca2f4e7a26323c8eabfdc
Matt Phelps <matt@leandme.com>
2024-06-10 10:19:25 +0200

    disallow test page

--- src/app/(site)/test/page.tsx
+++ src/app/(site)/test/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import prisma from '@/libs/prisma';
 import { getUserCount } from '@/libs/repositories/userRepository';
 
 const Test = async () => {
   const userCount = await getUserCount();
+  const userName = await prisma.user.findFirst()
 
   return (
     <>
-      <h1 className="text-xl">{userCount}</h1>
+      <h1 className="text-xl">{userName.name}</h1>
 
     </>
   );
