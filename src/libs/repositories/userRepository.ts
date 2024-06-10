07027f4c00b0de62191f8648d4a3c3795b253ec5
Matt Phelps <matt@leandme.com>
2024-06-10 12:23:22 +0200

    ok

--- src/libs/repositories/userRepository.ts
+++ src/libs/repositories/userRepository.ts
@@ -8,4 +8,7 @@ export const getUserCount = async () => {
     return prisma.user.findMany();
   };
   
-  
+  // Access like this
+  // import { getUserCount } from '@/libs/repositories/userRepository';
+  // const userCount = await getUserCount();
+  // const userName = await prisma.user.findFirst()
