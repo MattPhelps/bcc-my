df775c7b68472199ec813500fb2c8f631b5df921
Matt Phelps <matt@leandme.com>
2024-06-20 22:26:13 +0200

    lets go

--- src/components/CallToAction/index.tsx
+++ src/components/CallToAction/index.tsx
@@ -1,33 +1,27 @@
-import Image from "next/image";
-import Link from "next/link";
-import siteConfig from '../../../siteConfig';
 import Button from '@/components/Common/Button';
 
-const CallToAction = () => {
+const CallToAction = ({ heading, description, buttonText, buttonSlug }) => {
   return (
     <section>
       <div className="max-w-[1170px] mx-auto px-4 sm:px-8 xl:px-0">
         <div className="cta-box-gradient bg-dark rounded-[30px] relative overflow-hidden px-4 py-20 lg:py-25 z-999">
           {/* <!-- bg shapes --> */}
-
           <div className="absolute -z-1 bg-no-repeat bg-cover bg-bottom w-full h-full bottom-0 left-0 bg-[url(/images/cta/grid.svg)]"></div>
 
           <div className="wow fadeInUp text-center">
-           
             <h2 className="text-white mb-4.5 text-2xl font-extrabold sm:text-4xl xl:text-heading-2">
-            Join 5,645 tattoo enthusiasts
+              {heading}
             </h2>
             <p className="max-w-[714px] mx-auto font-medium mb-9">
-            Our community of ink lovers are already using the power of AI to create mindblowing designs.
-What are you waiting for?
+              {description}
             </p>
-
             <Button
-              href="/signup"
-              style={{ }}
+              href={buttonSlug}
+              onClick={false}
+              style={{}}
               className=""
-              >
-                Design your tattoo
+            >
+              {buttonText}
             </Button>
           </div>
         </div>
@@ -37,3 +31,4 @@ What are you waiting for?
 };
 
 export default CallToAction;
+
