c54b6fac0a6e0f178c8d234d77e322469aa4c0cb
Matt Phelps <matt@leandme.com>
2024-06-24 13:38:24 +0200

    update meaning

---
+++ src/app/(site)/meaning/[[...slug]]/page.tsx
@@ -0,0 +1,145 @@
+import CellGrid from "@/components/CellGrid";
+import Button from "@/components/Common/Button";
+import PageTitle from "@/components/Common/PageTitle";
+import { getAllMeanings, getMeaningsBySlug, getMeaningBySlug} from "@/libs/services/meaningService";
+import { Metadata } from "next";
+import Link from "next/link";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+export async function generateStaticParams() {
+  const meanings = await getAllMeanings();
+  const uniqueSlugs = [
+    ...new Set(meanings.map((meaning) => meaning.slug.split("/")[0])),
+  ]; // Assuming slugs follow a certain pattern
+  return uniqueSlugs.map((slug) => ({
+    slug: [slug],
+  }));
+}
+
+// export const metadata: Metadata = {
+//   title: "Meaning Detail",
+//   description: "Detailed view of a tattoo meanings",
+// };
+export async function generateMetadata({ params }) {
+  const { slug } = params;
+  
+  if (!slug) {
+    return {
+      title: "Meaning Detail",
+      description: "Detailed view of a tattoo meanings"
+    };
+  } else if (slug.length === 1) {
+    const categorySlug = slug[0];
+    const meanings = await getMeaningsBySlug(`/${categorySlug}`);
+    return {
+      title: `Meanings for ${categorySlug}`,
+      description: `Discover tattoo meanings in the category: ${categorySlug}`
+    };
+  } else {
+    const nestedSlug = slug.join('/');
+    const meaning = await getMeaningBySlug(nestedSlug);
+    if (meaning) {
+      return {
+        title: meaning.name,
+      };
+    } else {
+      return {
+        title: "Meaning not found",
+        description: "This tattoo meaning could not be found."
+      };
+    }
+  }
+}
+
+const MeaningPage = async ({ params }) => {
+  const { slug } = params;
+  if (!slug) {
+    // No slug provided, show all meanings
+    const allMeanings = await getAllMeanings();
+    return (
+      <>
+        <section className="pt-3 pb-25">
+          <div className="max-w-[1170px] text-[black]/80 dark:text-[white]/50 mx-auto px-4 sm:px-8 xl:px-0 z-10 pt-5">
+          <div className="flex justify-center">
+            <Breadcrumb />
+          </div>
+            <PageTitle
+              title="Tattoo Meanings"
+              paragraph="Find out the meaning behind all tattoo designs"
+            />
+            <div className="wow fadeInUp text-center">
+              <Button href="/design" style={{}} className="" onClick={false}>
+                Design your tattoo
+              </Button>
+            </div>
+            <div className="flex flex-wrap justify-center -mx-4 mt-24">
+              {allMeanings.length > 0 ? (
+                allMeanings.map((cell, index) => (
+                  <Link
+                    key={index}
+                    href={`meaning${cell.slug}`}
+                    className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 px-8 mb-8 duration-100 ease-in hover:scale-105"
+                  >
+                    <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+                      <img
+                        // src={cell.image}
+                        alt={cell.name}
+                        className="w-full h-56 object-cover object-center"
+                      />
+                      <div className="p-4">
+                        <h2 className="text-xl font-bold mb-2">{cell.name}</h2>
+                        <p className="text-gray-600 text-sm dark:text-gray-400">
+                          {/* {cell.description} */}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))
+              ) : (
+                <p>No meanings available.</p>
+              )}
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  } else if (slug.length === 1) {
+    // Single segment slug, show category of meanings
+    const categorySlug = slug[0];
+    const meanings = await getMeaningsBySlug(`/${categorySlug}`);
+    `/${slug}`;
+    if (!meanings.length) {
+      return <p>No meanings found for this category</p>;
+    }
+    return (
+      <>
+        <section className="pt-3 pb-25">
+          <div className="max-w-[1170px] text-[black]/80 dark:text-[white]/50 mx-auto px-4 sm:px-8 xl:px-0 z-10 pt-25">
+            <h1 className="text-3xl font-bold">Meanings for {categorySlug}</h1>
+            <CellGrid data={meanings} rootSlug={`/meaning/${categorySlug}`} />
+          </div>
+        </section>
+      </>
+    );
+  } else {
+    // Nested slug, show detailed meaning view
+    const nestedSlug = slug.join("/");
+    const meaning = await getMeaningBySlug(nestedSlug);
+    if (!meaning) {
+      return <p>Meaning not found</p>;
+    }
+    return (
+      <>
+        <section className="pt-3 pb-25">
+          <div className="max-w-[1170px] text-[black]/80 dark:text-[white]/50 mx-auto px-4 sm:px-8 xl:px-0 z-10 pt-25">
+            <h1 className="text-3xl font-bold">{meaning.name}</h1>
+            {/* <img src={style.image} alt={style.name} className="w-full h-full object-cover object-center mt-4" />
+            <p className="mt-4">{style.description}</p> */}
+          </div>
+        </section>
+      </>
+    );
+  }
+};
+
+export default MeaningPage;
--- src/libs/services/meaningService.ts
+++ src/libs/services/meaningService.ts
@@ -9,3 +9,31 @@ export const getAllMeanings = async () => {
     return [];
   }
 };
+
+export const getMeaningsBySlug = async (slug: string) => {
+  try {
+    const styles = await prisma.meaning.findMany({
+      where: {
+        slug: {
+          startsWith: slug, // Assuming the category is part of the slug
+        },
+      },
+    });
+    return styles;
+  } catch (error) {
+    console.error("Error fetching meaning by slug:", error);
+    return [];
+  }
+};
+
+export const getMeaningBySlug = async (slug: string) => {
+  try {
+    const style = await prisma.meaning.findUnique({
+      where: { slug },
+    });
+    return style;
+  } catch (error) {
+    console.error("Error fetching meaning:", error);
+    return null;
+  }
+};
